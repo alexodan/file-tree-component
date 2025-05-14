80ebe6c7aa150b1a6b4750555cbbc7868e33176d
Alexis Diaz <alexdan1993@gmail.com>
2025-05-14 09:03:25 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { filetree } from "./constants.ts";
 import { Node } from "./TreeNode.tsx";
 import { isFolder } from "./utils.ts";
@@ -13,18 +13,31 @@ export type TreeViewProps = {
 
 function TreeView({ tree }: TreeViewProps) {
   const [isOpen, setIsOpen] = useState<Record<string, boolean>>({});
-  console.log(isOpen);
+  const [isAnimationInProgress, setIsAnimationInProgress] = useState(false);
+  const animationRef = useRef<number>(null);
 
   const handleToggleExpand = (id: string) => {
-    // setIsOpen((op) => !op);
+    console.log("toggle expand");
     setIsOpen((prev) => ({
       ...prev,
       [id]: !prev[id],
     }));
+    setIsAnimationInProgress(true);
+    animationRef.current = setInterval(() => {
+      setIsAnimationInProgress(false);
+    }, 500);
   };
 
+  useEffect(() => {
+    return () => {
+      if (animationRef.current) {
+        clearTimeout(animationRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <ul className={`tree`}>
+    <ul className="tree">
       {tree.map((node) => {
         if (isFolder(node)) {
           return (
@@ -39,12 +52,14 @@ function TreeView({ tree }: TreeViewProps) {
               </span>
               <div
                 className="tree-wrap"
-                style={{
-                  height: isOpen[node.name]
-                    ? `${(node.nodes?.length ?? 0) * 20}px`
-                    : "0",
-                }}
-                // pass inlines
+                // style={{
+                //   height: isOpen[node.name]
+                //     ? `${(node.nodes?.length ?? 0) * 20}px`
+                //     : "0",
+                // }}
+                data-is-closed={!isOpen[node.name]}
+                data-is-animating={isOpen[node.name] && isAnimationInProgress}
+                data-is-open={isOpen[node.name] && !isAnimationInProgress}
               >
                 {isOpen[node.name] && <TreeView tree={node.nodes ?? []} />}
               </div>
